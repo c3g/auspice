6b7bd2b81e83a3beb1caa9dd8bb4b0b13715799a
Rom Grk <romgrk.cc@gmail.com>
2020-05-15 11:24:10 -0400

    l11n: lint

--- src/translations/en.js
+++ src/translations/en.js
@@ -7,16 +7,16 @@ const strings = {
 
     title1: "Real-time tracking of Quebec SARS-CoV-2 evolution",
     subtitle1: <>
-        CoVSeQ is a partnership between
-        the <a href="">Institut National de Sant&eacute; Publique du Qu&eacute;bec (INSPQ)</a> and
-        the <a href="">McGill Genome Center</a> to sequence the viral genome of Quebec
-        patients with COVID-19 disease. The viral samples are taken from a Quebec viral
-        biobank, termed the CoVBanQ, which is hosted in
-        the <a href="">Laboratoire de Sant&eacute; Publique du Qu&eacute;bec (LSPQ)</a>.
-        The website is based on <a href="">Nextstrain</a>, an open-source project to
-        harness the scientific and public health potential of pathogen genome data. It
-        provides powerful analytic and visualization tools to aid epidemiological
-        understanding and improve outbreak response.
+      CoVSeQ is a partnership between
+      the <a href="">Institut National de Sant&eacute; Publique du Qu&eacute;bec (INSPQ)</a> and
+      the <a href="">McGill Genome Center</a> to sequence the viral genome of Quebec
+      patients with COVID-19 disease. The viral samples are taken from a Quebec viral
+      biobank, termed the CoVBanQ, which is hosted in
+      the <a href="">Laboratoire de Sant&eacute; Publique du Qu&eacute;bec (LSPQ)</a>.
+      The website is based on <a href="">Nextstrain</a>, an open-source project to
+      harness the scientific and public health potential of pathogen genome data. It
+      provides powerful analytic and visualization tools to aid epidemiological
+      understanding and improve outbreak response.
     </>,
     button1: "Lire",
 
