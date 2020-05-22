613edb6f6b11e913503b55343e1883836432f361
P-O Quirion <pioliqui@gmail.com>
2020-05-22 01:47:55 -0400

    tweak

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
   mkdir -p ~/.ssh 
   echo -e "$SSH_CLEF" | base64 -d > ~/.ssh/id_rsa 
   chmod og-rwx ~/.ssh/id_rsa 
-  ssh-keyscan -H covseq.ca >> >> ~/.ssh/known_hosts 
+  ssh-keyscan -H covseq.ca >> ~/.ssh/known_hosts 
   ssh covsec.ca ./update_auspice.sh 
 fi
