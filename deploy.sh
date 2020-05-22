ecb9f1b11e11e5e9541473f5bcd7ca9cf31d1a77
P-O Quirion <pioliqui@gmail.com>
2020-05-22 01:43:28 -0400

    deploy script and only one js version

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+  mkdir -p ~/.ssh 
+  echo -e "$SSH_CLEF" | base64 -d > ~/.ssh/id_rsa 
+  chmod og-rwx ~/.ssh/id_rsa 
+  ssh-keyscan -H covseq.ca >> >> ~/.ssh/known_hosts 
+  ssh covsec.ca ./update_auspice.sh 
+fi
