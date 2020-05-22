cb456f476488fa12a1a34df6e7c56b5da78ee147
P-O Quirion <pioliqui@gmail.com>
2020-05-22 10:45:59 -0400

    more debug for deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ "$TRAVIS_BRANCH" == "master" ]; then
   mkdir -p ~/.ssh 
-  echo -n "$SSH_CLEF" |  tr ' ' '\n' | base64 -d > ~/.ssh/id_rsa 
+  echo -n "${SSH_CLEF}" |  tr ' ' '\n' | base64 -d > ~/.ssh/id_rsa 
   chmod og-rwx ~/.ssh/id_rsa 
   ssh-keyscan -H covseq.ca >> ~/.ssh/known_hosts 
   ssh covseq.ca ./update_auspice.sh 
